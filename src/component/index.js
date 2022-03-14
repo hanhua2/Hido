febfaace6c0d5aa39ce90a55dee662ee5c09784d
Kumamto <100348989+Kumamto@users.noreply.github.com>
2022-03-14 13:11:03 +0800

    Update index.js

--- src/component/index.js
+++ src/component/index.js
@@ -1,4 +1,6 @@
 export { default as Home } from "./Home";
 export { default as LogIn } from "./LogIn";
-export { default as Page1 } from "./Page1";
 export { default as Navigation } from "./Navigation";
+export { default as Detail } from "./Detail";
+export { default as Day } from "./Day";
+export { default as Month } from "./Month";
